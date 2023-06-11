eb25e78cbfbdf590dca402933e054b34eb61d4c3
Jordan <j.r.noble@live.co.uk>
2023-06-11 12:31:18 +0100

    renders icons and dates

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -4,7 +4,7 @@ import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
   const validProps = {
-    date: 1111111,
+    date: 1525046400000,
     description: "Stub description",
     icon: "stubIcon",
     temperature: {
@@ -22,7 +22,21 @@ describe("ForecastSummary", () => {
         temperature={validProps.temperature.max}
       />,
     );
-
     expect(asFragment()).toMatchSnapshot();
+  });
+  it("renders correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date="Mon Apr 30 2018"
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature.max}
+      />,
+    );
+    expect(getByTestId("forecast-summary")).toHaveClass("forecast-summary");
+    expect(getByText("Mon Apr 30 2018")).toHaveClass("forecast-summary_date");
+    expect(getByText("Stub description")).toHaveClass("forecast-summary_description");
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary_icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary_temperature");
   });
 });
