3e623540125796ce44c8b9003c2bcb32560013c3
Jordan <j.r.noble@live.co.uk>
2023-06-18 17:10:19 +0100

    styling finishing touches

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -7,7 +7,7 @@ function LocationDetails(props) {
   if (errorMessage) {
     return <h1 className="location-details">{errorMessage}</h1>;
   }
-  return <h1 className="location-details">{`${city}, ${country}`}</h1>;
+  return <h1 className="location-details">{`Five day forecast for ${city}, ${country}`}</h1>;
 }
 
 export default LocationDetails;
